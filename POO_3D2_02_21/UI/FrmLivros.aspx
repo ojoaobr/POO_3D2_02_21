1b93a342bd11895e37dbc07fb69b9c659b9fafb2
Joao Pedro <joaopedroaf03@gmail.com>
2020-09-12 12:12:06 -0300

    Criação da pasta Content, Adição do Bootstrap

--- POO_3D2_02_21/UI/FrmLivros.aspx
+++ POO_3D2_02_21/UI/FrmLivros.aspx
@@ -7,13 +7,43 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Livraria A&J</title>
 </head>
-<body>
+<body class="align-content-center">
     <form id="form1" runat="server">
-        <div>
+            <div class="jumbotron">
+           
+           <h1 class="h3 mb-3 font-weight-normal">Clientes</h1>
 
-            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+             <asp:Label ID="msgerro" runat="server" ForeColor="Red" Text="." Visible="false"></asp:Label>
+              <br />
+             <asp:Label ID="lblNome" runat="server" Text="Nome "></asp:Label>
+             <asp:TextBox ID="txtNome" type="text" runat="server" CssClass="form-control" ></asp:TextBox>
 
+             <asp:Label ID="lblSobrenome" runat="server" Text="SobreNome "></asp:Label>
+             <asp:TextBox ID="txtSobrenome" type="text" runat="server" CssClass="form-control" ></asp:TextBox>
+
+             <asp:Label ID="Label2" runat="server" Text="CPF "></asp:Label>
+             <asp:TextBox ID="txtCpf" type="text" runat="server" CssClass="form-control" ></asp:TextBox>
+
+             <asp:Label ID="Label3" runat="server" Text="Email "></asp:Label>
+             <asp:TextBox ID="txtEmail" type="text" runat="server" CssClass="form-control" ></asp:TextBox>
+
+             <asp:Label ID="Label1" runat="server" Text="Senha"></asp:Label>
+             <asp:TextBox ID="txtSenhaAtual" type="password" runat="server" CssClass="form-control" ></asp:TextBox>
+
+              <asp:Label ID="Label4" runat="server" Text="Tipo do Usuário"></asp:Label>
+              <asp:DropDownList ID="drpTipoUsuario" runat="server" CssClass="form-control"></asp:DropDownList>
+  
+           
+            
+       <br />
+                    <Columns>
+                        
+                    </Columns>
+                    <PagerSettings Position="TopAndBottom" />
+                </asp:GridView>
+      
         </div>
+  
     </form>
 </body>
 </html>
--- POO_3D2_02_21/UI/FrmLivros.aspx.designer.cs
+++ POO_3D2_02_21/UI/FrmLivros.aspx.designer.cs
@@ -24,6 +24,87 @@ namespace POO_3D2_02_21
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Controle msgerro.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label msgerro;
+
+        /// <summary>
+        /// Controle lblNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNome;
+
+        /// <summary>
+        /// Controle txtNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNome;
+
+        /// <summary>
+        /// Controle lblSobrenome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSobrenome;
+
+        /// <summary>
+        /// Controle txtSobrenome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSobrenome;
+
+        /// <summary>
+        /// Controle Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Controle txtCpf.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCpf;
+
+        /// <summary>
+        /// Controle Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Controle txtEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
         /// Controle Label1.
         /// </summary>
         /// <remarks>
@@ -31,5 +112,32 @@ namespace POO_3D2_02_21
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Controle txtSenhaAtual.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSenhaAtual;
+
+        /// <summary>
+        /// Controle Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Controle drpTipoUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList drpTipoUsuario;
     }
 }
