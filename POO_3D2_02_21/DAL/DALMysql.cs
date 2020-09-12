ad8261cc83c54bcb29f4091469511b8e452ff0ac
Joao Pedro <joaopedroaf03@gmail.com>
2020-09-12 11:43:04 -0300

    Criação das classes DTO e DAL do projeto

---
+++ POO_3D2_02_21/DAL/DALMysql.cs
@@ -0,0 +1,70 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace POO_3D2_02_21.DAL
+{
+    public class DALMysql
+    {
+
+        private MySqlConnection conexao;
+        private string string_conexao = "Persist security info = false;" +
+                                         "server=localhost;" +
+                                         "database=bd_Livraria;" +
+                                         "user=root ; pwd=;";
+
+        public void conectar()
+        {
+            try
+            {
+                conexao = new MySqlConnection(string_conexao);
+                conexao.Open();
+            }
+            catch (MySqlException e)
+            {
+                throw new Exception("Problemas de conexão com banco de dados. \nErro: " + e.Message);
+            }
+        }
+
+        public void executarComando(string sql)
+        {
+            try
+            {
+                conectar();
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+                comando.ExecuteNonQuery();
+            }
+            catch (MySqlException e)
+            {
+                throw new Exception("Não foi possível executar a instrução no Banco de Dados.. Erro: " + e.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public DataTable executarConsulta(string sql)
+        {
+            try
+            {
+                conectar();
+                DataTable dt = new DataTable();
+                MySqlDataAdapter dados = new MySqlDataAdapter(sql, conexao);
+                dados.Fill(dt);
+                return dt;
+            }
+            catch (MySqlException e)
+            {
+                throw new Exception("Não foi possível selecionar os registros no Banco de Dados. Erro: " + e.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
---
+++ POO_3D2_02_21/DTO/tblLivrosDTO.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POO_3D2_02_21.DTO
+{
+    public class tblLivrosDTO
+    {
+        private int idLivro, idAutor, idEditora;
+        private string titulo, numPaginas;
+        private DateTime dataCadastro;
+        private double valor;
+
+        public int IdLivro { get => idLivro; set => idLivro = value; }
+        public int IdAutor { get => idAutor; set => idAutor = value; }
+        public int IdEditora { get => idEditora; set => idEditora = value; }
+
+        public DateTime DataCadastro { get => dataCadastro; set => dataCadastro = value; }
+
+        public double Valor { get => valor; set => valor = value; }
+
+        public string Titulo
+        {
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.titulo = value;
+                }
+                else
+                {
+                    throw new Exception("O campo Título é obrigatório");
+                }
+
+            }
+            get { return this.titulo; }
+
+        }
+
+        public string num_Paginas
+        {
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.numPaginas = value;
+                }
+                else
+                {
+                    throw new Exception("O campo Número de Páginas é obrigatório");
+                }
+
+            }
+            get { return this.numPaginas; }
+
+        }
+
+    }
+}
